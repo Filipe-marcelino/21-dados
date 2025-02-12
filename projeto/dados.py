7cd65e7bb360566b5e1865230cd00b327db576de
Filipin <160949589+Filipe-marcelino@users.noreply.github.com>
2025-02-12 16:13:31 -0300

    Update dados.py

--- projeto/dados.py
+++ projeto/dados.py
@@ -1,17 +1,36 @@
 import random
 
 class jogador:
-    def __init__(self,pontos):
-        self.pontos = pontos
+    def __init__(self,pontosplayer: int):
+        self.pontosplayer = pontosplayer
 
     def Jogar(self):
         dados = random.randint(1,6)
+        self.pontosplayer += dados
         print(dados)
+        print(self.pontosplayer)
 
 class adversário:
-    def __init__(self,pontos):
-        self.pontos = pontos
+    def __init__(self,pontospc: int):
+        self.pontospc = pontospc
 
     def Jogar(self):
         dadospc = random.randint(1,6)
+        self.pontospc += dadospc
 
+player1 = jogador(0)
+player2 = adversário(0)
+rodada = str(input('Você deseja jogar um dado? s/n'))
+while rodada.upper() == "S":
+    player1.Jogar()
+    player2.Jogar()
+    rodada = str(input('Você deseja jogar outro dado? s/n'))
+
+    if player1.pontosplayer >= 21:
+        break
+
+if player1.pontosplayer == 21:
+    print('Você venceu!!')
+
+elif player2.pontospc == 21:
+    print('Você perdeu')
