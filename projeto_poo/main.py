f540872499b50420da39d7b2ed66fa149f7c5f89
Cecília_Medeiros <161476484+ciliadeiros@users.noreply.github.com>
2025-03-08 15:57:32 -0300

    Update main.py

--- projeto_poo/main.py
+++ projeto_poo/main.py
@@ -39,92 +39,88 @@ def main():
         print(Back.WHITE + '    ', "▓▓▓▓▓▓▓▓▓   ▓▓░  ░▓▓   ░▓▓▓▓▓▓▓        ░▓▓         ░▓▓▓▓   ░▓▓▓▓   ▓▓░  ░▓▓  ▓  ▓  ▓▓▓▓▓▓▓▓▓",Back.WHITE + '    ')
         print(Fore.YELLOW + ' ▦ ', " ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓",Fore.YELLOW + ' ▦ ')
         print(Back.WHITE + '    ', Fore.YELLOW + '▦ ', Back.WHITE + '     ', Fore.YELLOW + '▦ ', Back.WHITE + '     ', Fore.YELLOW + '▦ ', Back.WHITE + '     ', Fore.YELLOW + '▦ ', Back.WHITE + '     ', Fore.YELLOW + '▦ ', Back.WHITE + '     ', Fore.YELLOW + '▦ ', Back.WHITE + '     ', Fore.YELLOW + '▦ ', Back.WHITE + '     ', Fore.YELLOW + '▦ ', Back.WHITE + '     ', Fore.YELLOW + '▦ ', Back.WHITE + '     ', Fore.YELLOW + '▦ ', Back.WHITE + '     ', Fore.YELLOW + '▦ ', Back.WHITE + '    ')
-        
-        contador = 0
+
         num_rodadas = input('\nEm quantas rodadas você acha que vai ganhar? ')
         while not num_rodadas.isdigit():
             print('\nOps! Precisa ser um número... Tente novamente! :)')
             num_rodadas = input('Em quantas rodadas você acha que vai ganhar? ')
         
         num_rodadas = int(num_rodadas)
-        for n in range(0, num_rodadas):
+        for n in range(0, num_rodadas+1):
             jogador.aposta()
             computador.aposta()
+
+            if n > 1:
+                if jogador.fichas == 0:
+                    print('PC WINS!')
+                    print(Style.RESET_ALL + Back.RED + "\nPONTUAÇÃO FINAL:" + Style.RESET_ALL)
+                    print(f"{jogador.nome}: {jogador.pontos} pontos")
+                    print(f"Computador: {computador.pontos} pontos")
+                    break
+                
+                elif computador.fichaspc == 0:
+                    print('PLAYER WINS!')
+                    print(Style.RESET_ALL + Back.RED + "\nPONTUAÇÃO FINAL:" + Style.RESET_ALL)
+                    print(f"{jogador.nome}: {jogador.pontos} pontos")
+                    print(f"Computador: {computador.pontos} pontos")
+                    break
+
+                else:
+                    continue
 
             jogador.pontos = 0
             computador.pontos = 0
 
-            jogador.Jogar_dado()
-            print(Style.RESET_ALL + Fore.YELLOW + '---------------------------------' + Style.RESET_ALL)
+            while True:
+                jogador.Jogar_dado()
+                print(Style.RESET_ALL + Fore.YELLOW + '_________________________________' + Style.RESET_ALL)
 
-            computador.Jogar_dado()
+                computador.Jogar_dado()
 
-            if jogador.pontos > 21:
-                computador.fichaspc = computador.fichaspc + (jogador.apostadas*2)
-                print('\n🖥️ PC WINS!')
-                print('')
-                break                   
-
-            elif jogador.pontos == 21:
-                contador += 1
-                jogador.fichas = jogador.fichas + (jogador.apostadas*2)
-                print('\n PLAYER WINS!')
-                break
-
-            elif computador.pontos > 21:
-                contador += 1
-                jogador.fichas = jogador.fichas + (jogador.apostadas*2)
-                print('\n PLAYER WINS!')
-                break
-
-            elif computador.pontos== 21:
-                computador.fichaspc = computador.fichaspc + (jogador.apostadas*2)
-                print('\n🖥️ PC WINS!')
-                break
-                
-                # EM CASO DE EMPATE
-            elif jogador.pontos > 21 and computador.pontos > 21:
-                if computador.pontos == jogador.pontos:
-                    print('EMPATE!')
-                elif computador.pontos > jogador:
-                    contador += 1
-                    jogador.fichas = jogador.fichas + (jogador.apostadas*2)
-                    print(' PLAYER WINS!')
-                else:
+                if jogador.pontos > 21:
                     computador.fichaspc = computador.fichaspc + (jogador.apostadas*2)
-                    print('🖥️ PC WINS!')
-            else:
-                continue  # Se ninguém ganhou, continua o jogo
+                    print('\n🖥️ PC WINS!')
+                    print('')
+                    break                   
 
-        if jogador.fichas == 0:
-            print('PC WINS!')
-            print("\nPontuação Final:")
-            print(f"{jogador.nome}: {jogador.pontos} pontos")
-            print(f"Computador: {computador.pontos} pontos")
-           
-        elif computador.fichaspc == 0:
-            print(' PLAYER WINS!')
-            print("\nPontuação Final:")
-            print(f"{jogador.nome}: {jogador.pontos} pontos")
-            print(f"Computador: {computador.pontos} pontos")
+                elif jogador.pontos == 21:
+                    jogador.fichas = jogador.fichas + (jogador.apostadas*2)
+                    print('\nPLAYER WINS!')
+                    break
+
+                elif computador.pontos > 21:
+                    jogador.fichas = jogador.fichas + (jogador.apostadas*2)
+                    print('\nPLAYER WINS!')
+                    break
+
+                elif computador.pontos== 21:
+                    computador.fichaspc = computador.fichaspc + (jogador.apostadas*2)
+                    print('\n🖥️ PC WINS!')
+                    break
+                    
+                    # EM CASO DE EMPATE
+                elif jogador.pontos > 21 and computador.pontos > 21:
+                    if computador.pontos == jogador.pontos:
+                        print('EMPATE!')
+                    elif computador.pontos > jogador:
+                        jogador.fichas = jogador.fichas + (jogador.apostadas*2)
+                        print('PLAYER WINS!')
+                    else:
+                        computador.fichaspc = computador.fichaspc + (jogador.apostadas*2)
+                        print('🖥️ PC WINS!')
+                else:
+                    continue  # Se ninguém ganhou, continua o jogo
             
-        elif contador >= num_rodadas/2:
-            print(" PLAYER WINS!")
-            print("\nPontuação Final:")
-            print(f"{jogador.nome}: {jogador.pontos} pontos")
-            print(f"Computador: {computador.pontos} pontos")
-            
-        else:
-            print('🖥️ PC WINS')
-            print("\nPontuação Final:")
-            print(f"{jogador.nome}: {jogador.pontos} pontos")
-            print(f"Computador: {computador.pontos} pontos")
-                
-    elif modo.upper() == "CLASSICO":
+            print(Style.RESET_ALL)
+            print(Back.RED + "PONTUAÇÃO FINAL:", Style.RESET_ALL)
+            print(f"{(jogador.nome).upper()}: {jogador.pontos} pontos")
+            print(f"COMPUTADOR: {computador.pontos} pontos")  
+
+    elif modo.upper() == "CLASSICO" or modo.upper() == "CLÁSSICO":
 
         while True:
             jogador.Jogar_dado()
-            print(Style.RESET_ALL + Fore.YELLOW + '---------------------------------' + Style.RESET_ALL)
+            print(Style.RESET_ALL + Fore.YELLOW + '_________________________________' + Style.RESET_ALL)
             computador.Jogar_dado()
 
             if jogador.pontos > 21:
@@ -133,11 +129,11 @@ def main():
                 break
 
             elif jogador.pontos == 21:
-                print('\n PLAYER WINS!')
+                print('\nPLAYER WINS!')
                 break
 
             elif computador.pontos > 21:
-                print('\n PLAYER WINS!')
+                print('\nPLAYER WINS!')
                 break
 
             elif computador.pontos== 21:
@@ -149,16 +145,18 @@ def main():
                 if computador.pontos == jogador.pontos:
                     print('EMPATE!')
                 elif computador.pontos > jogador:
-                    print(' PLAYER WINS!')
+                    print('PLAYER WINS!')
                 else:
                     print('🖥️ PC WINS!')
             else:
                 continue  # Se ninguém ganhou, continua o jogo
 
             # PONTUAÇÃO FINAL
-        print("\nPontuação Final:")
-        print(f" {jogador.nome}: {jogador.pontos} pontos")
-        print(f"🖥️ Computador: {computador.pontos} pontos")  
+        
+        print()
+        print(Back.RED + "PONTUAÇÃO FINAL:", Style.RESET_ALL)
+        print(f"{(jogador.nome).upper()}: {jogador.pontos} pontos")
+        print(f"COMPUTADOR: {computador.pontos} pontos")  
 
 if __name__ == "__main__":
     main()
